6ccb7d29e9d3e88206191759e44aefb3170388bd
ooakind <gywls0724@naver.com>
2021-01-06 17:53:19 +0900

    Set durations in base sequence concatenation

--- base_generator.py
+++ base_generator.py
@@ -1,5 +1,4 @@
 # Base generator
-
 import ast
 
 from magenta.models.melody_rnn import melody_rnn_sequence_generator
@@ -9,16 +8,18 @@ from note_seq.protobuf import generator_pb2
 from note_seq.protobuf import music_pb2
 from note_seq.sequences_lib import concatenate_sequences
 from note_seq import Melody
+import note_seq
 from melody_generator import melody_generator
 
 def base_generator(roots_sequence, num_steps=16, temperature=0.5, qpm=120, chords_per_bar=1):
     base_sequences_list = list()
     for n in roots_sequence.notes:
         for i in range(chords_per_bar):
-            root_melody = Melody(ast.literal_eval("[" + str(n.pitch - 12) +"]"))
+            root_melody = Melody(ast.literal_eval("[" + str(n.pitch - 24) +"]"))
             root_sequence = root_melody.to_sequence(qpm=qpm)
             base_chunk = melody_generator(root_sequence, num_steps, temperature, qpm)
             base_sequences_list.append(base_chunk)
-        
-    base_sequence = concatenate_sequences(base_sequences_list)
+
+    durations = [4*60/qpm] * len(roots_sequence.notes)
+    base_sequence = concatenate_sequences(base_sequences_list, durations)
     return base_sequence
