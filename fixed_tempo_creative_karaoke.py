39408e4ab154b7eaad3570ddb2f94819294aba28
ooakind <gywls0724@naver.com>
2021-01-09 18:32:24 +0900

    Add midi_to_wav & seq_to_midi_with_program utils

--- fixed_tempo_creative_karaoke.py
+++ fixed_tempo_creative_karaoke.py
@@ -3,19 +3,19 @@ import note_seq
 
 from melody_generator import melody_generator
 from base_generator import base_generator
-from fixed_tempo_drum_generator import drum_generator
+from drum_generator import drum_generator
 from chord_extraction import extract_roots_as_note_seq, extract_chords_from_note_sequence
 from wav_utils import wav_to_midi_with_tempo
-from utils import combine_note_sequence_as_midi
+from utils import combine_note_sequence_as_midi, midi_to_wav, seq_to_midi_with_program
 # from beat_analysis import 
 
 # input 멜로디(wav)의 path 받아서 처리
-def creative_karaoke(filepath, tempo=60):
+def creative_karaoke(filepath, tempo=60, inst=[53, 0, 36, 118]):
     # constants
     total_num_steps = 256
     outfilepath = './results/user_midi.mid'
 
-    wav_to_midi(filepath, tempo, False, outfilepath) # atmm 사용 여부 선택 가능, 선택 하는 것으로 일단 작성
+    wav_to_midi_with_tempo(filepath, tempo, False, outfilepath) # atmm 사용 여부 선택 가능, 선택 하는 것으로 일단 작성
     user_sequence = note_seq.midi_file_to_note_sequence(outfilepath)
     user_sequence.tempos[0].qpm = tempo
 
@@ -43,16 +43,13 @@ def creative_karaoke(filepath, tempo=60):
     results_seq.append(drum)
 
 # 각각 미디를 파일로 출력
+    #inst = [53, 0, 36, 118]# set program number
+    str_name_dict = {0: 'main_melody', 1: 'chords', 2: 'base', 3: 'drum'}
     combine_note_sequence_as_midi(results_seq, './results/mid/fixed_tempo_result_combined_midi.mid')
 
-    '''
-    note_seq.sequence_proto_to_midi_file(main_melody, 'result_main_melody.mid')
-    note_seq.sequence_proto_to_midi_file(chords, 'result_chord.mid')
-    note_seq.sequence_proto_to_midi_file(base, 'result_base.mid')
-    note_seq.sequence_proto_to_midi_file(drum, 'result_beat.mid')
-    '''
-
-
+    for i in range(len(results_seq)):
+        midi = seq_to_midi_with_program(results_seq[i], inst[i])
+        midi_to_wav(midi, './results/wav/%s.wav' % str_name_dict[i])
 
 if __name__== "__main__":
     creative_karaoke('./data/wav/user_input.wav', 120)
