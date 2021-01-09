77a528d6de8dddfe5c6327a613426afe32fafb14
ooakind <gywls0724@naver.com>
2021-01-09 16:35:55 +0900

    Add utils for fixed tempo version

--- wav_utils.py
+++ wav_utils.py
@@ -1,13 +1,14 @@
 from numpy.core.shape_base import atleast_1d
 from pydub import AudioSegment
 import os
-from utils import tempo_and_onset
+from utils import tempo_and_onset, get_onset
 import crepe
 import csv
 import pitch_detection
 import note_seq
 from audio_to_midi_melodia import audio_to_midi_melodia
 import visual_midi
+import beat_analysis
 
 def trim_wav(filepath, start_time=0, end_time=-1):
       """
@@ -67,6 +68,52 @@ def wav_to_midi(filepath, use_atmm=False , outfile = None, _smooth=0.25, _mindur
                               smooth=_smooth, minduration=_minduration)
       else:
             # When not using atmm, algorithm in pitch_detection will be used as default
+            note_seq.note_sequence_to_midi_file(seq,midi_filepath)
+
+      print(midi_filepath+' saved from '+filepath)
+
+# NOTE: Changed to Fixed Tempo version(return)
+def wav_to_midi_with_tempo(filepath, tempo, use_atmm=False, outfile = None, _smooth=0.25, _minduration=0.1):
+      """
+      Converts Wav input file into Midi
+      Option to use atmm, currently false by default
+      if not using atmm, uses algorithm in pitch_detection
+
+      """
+      
+      if outfile is not None:
+            midi_filepath = outfile
+      else:
+            midi_filepath = filepath.split('.')[0] + '.mid'
+
+      # Use crepe to extract frequency and confidence for each time step
+      csv_file = 'results/crepe/'+filepath.split('/')[-1].split('.')[0]+'.f0.csv'
+      if not os.path.isfile(csv_file):
+        crepe.process_file(filepath,output='results/crepe')
+
+      # Read f0.csv file
+      frequency = []
+      confidence = []
+      with open(csv_file,newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+          frequency.append(float(row['frequency']))
+          confidence.append(float(row['confidence']))
+
+      # Convert into NoteSequence and do basic trimming
+      seq = pitch_detection.crepe_to_note_sequenece(frequency, confidence, trills=True)
+      seq_to_mid = note_seq.note_sequence_to_pretty_midi(seq)
+      estimated_start = get_onset(seq_to_mid, tempo)
+     
+      if use_atmm:
+            # Extract tempo from midi - needed as an argument for atmm
+            # use atmm to convert wav into midi
+            audio_to_midi_melodia.audio_to_midi_melodia(filepath, midi_filepath, tempo,
+                              smooth=_smooth, minduration=_minduration)
+      else:
+            # When not using atmm, algorithm in pitch_detection will be used as default
+            beat_analysis.adjust_sequence_to_onset(seq, estimated_start)
+            beat_analysis.set_tempo(seq, tempo)
             note_seq.note_sequence_to_midi_file(seq,midi_filepath)
 
       print(midi_filepath+' saved from '+filepath)
