07a854a83064c37e36626d5008fd71fc30da2f84
jen <RisingStockPrices@github.com>
2021-01-11 16:07:10 +0900

    Replace RnB with Disco genre

--- fixed_tempo_creative_karaoke.py
+++ fixed_tempo_creative_karaoke.py
@@ -6,17 +6,17 @@ from base_generator import base_generator
 from drum_generator import drum_generator
 from chord_extraction import extract_roots_as_note_seq, extract_chords_from_note_sequence, extract_chords_and_roots_from_note_sequence
 from wav_utils import wav_to_midi_with_tempo
-from utils import combined_sequence_to_midi_with_instruments, midi_to_wav, seq_to_midi_with_program
+from utils import combined_sequence_to_midi_with_instruments, midi_to_wav, seq_to_midi_with_program, transpose_note_sequence
 from note_seq.sequences_lib import stretch_note_sequence
 from constant import *
 
 # input 멜로디(wav)의 path 받아서 처리
-# melody, chords, bass, drums
-genre_inst = {GENRE_JAZZ:[65,0,36,40],GENRE_RNB:[100,53,35,40],GENRE_CLASSICAL:[0,48,43,61], GENRE_ROCK:[29,18,34,0]}
-genre_drum = {GENRE_JAZZ:"120 Bossa Hihat.mid",GENRE_RNB:"160 New Orleans Feel Ride 001 (16).mid",GENRE_CLASSICAL:"120 Bossa Hihat.mid", GENRE_ROCK:"70 Ridebell sync groove.mid"}
-genre_str = {GENRE_JAZZ:"Jazz",GENRE_RNB:"RnB",GENRE_CLASSICAL:"Classical", GENRE_ROCK:"Rock"}
+# melody, chords, bass, drums [100,53,35,40] 18
+genre_inst = {GENRE_JAZZ:[65,0,36,40],GENRE_DISCO:[54,53,36,40],GENRE_CLASSICAL:[40,48,43,61], GENRE_ROCK:[29,5,34,0]}
+genre_drum = {GENRE_JAZZ:"120 Bossa Hihat.mid",GENRE_DISCO:"130 DISCOA.mid",GENRE_CLASSICAL:"120 Bossa Hihat.mid", GENRE_ROCK:"70 Ridebell sync groove.mid"}
+genre_str = {GENRE_JAZZ:"Jazz",GENRE_DISCO:"Disco",GENRE_CLASSICAL:"Classical", GENRE_ROCK:"Rock"}
 
-def creative_karaoke(filepath, tempo=60, genre = GENRE_JAZZ, velocity = [90,50,50,50]):
+def creative_karaoke(filepath, tempo=60, genre = GENRE_JAZZ, velocity = [80,50,90,90]):
     # constants
     total_num_steps = 256
     outfilepath = './results/user_midi.mid'
@@ -35,11 +35,12 @@ def creative_karaoke(filepath, tempo=60, genre = GENRE_JAZZ, velocity = [90,50,5
     quantized_main_melody = note_seq.sequences_lib.quantize_note_sequence(main_melody, steps_per_quarter=4)
 # 코드, 베이스, 드럼 추출
 # 코드
-    chords, roots = extract_chords_and_roots_from_note_sequence(quantized_main_melody)
+    chords, roots = extract_chords_and_roots_from_note_sequence(quantized_main_melody, maj_min=True, triads=(genre != GENRE_JAZZ))
     results_seq.append(chords)
 
 # 베이스
     bass = base_generator(roots, 16, 0.7, quantized_main_melody.tempos[0].qpm)
+    #transpose_note_sequence(bass, 1)
     results_seq.append(bass)
 
 # 드럼
@@ -65,7 +66,7 @@ def creative_karaoke(filepath, tempo=60, genre = GENRE_JAZZ, velocity = [90,50,5
         results_seq.append(drum)
     else: 
         results_seq.append(roots)
-        velocity = [90,50,50,90]
+        velocity = [90,50,50,50]
 
 # 각각 미디를 파일로 출력
     str_name_dict = {0: 'main_melody', 1: 'chords', 2: 'base', 3: 'drum'}
@@ -76,7 +77,7 @@ def creative_karaoke(filepath, tempo=60, genre = GENRE_JAZZ, velocity = [90,50,5
         midi_to_wav(midi, './results/fixed/%s_%s_%s.wav' % (title, genre_str[genre], str_name_dict[i]))
 
 if __name__== "__main__":
-    genre = GENRE_CLASSICAL
+    genre = GENRE_JAZZ
     tempo = genre_drum[genre].split(" ")[0]
     folder = 'data/wav/%s/' % tempo
     for file in os.listdir(folder):
